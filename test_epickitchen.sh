961ebfa00a29e7de9bd6fed2331295a6a034cfd2
BolinLai <lai.b.bryan@gmail.com>
2024-08-15 09:33:46 +0000

    fix bug

--- test_epickitchen.sh
+++ test_epickitchen.sh
@@ -3,7 +3,7 @@
 
 echo Path:$1
 
-config='configs/generate_kitchen.yaml'
+config='configs/generate_epickitchen.yaml'
 
 for i in 1 2 3 4 5 6 7 8; do
     sbatch sbatch_inference/epickitchen_inference/test_epickitchen_sbatch_$i.sh $config $1
